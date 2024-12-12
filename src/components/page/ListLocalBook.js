8683c5085fb9de92c9c25aedd430d6f29b22530f
kimjavascript <wnstjd343442@gmail.com>
2024-12-12 21:47:17 +0900

    검색기능 및 모달 구현 완료

--- src/components/page/ListLocalBook.js
+++ src/components/page/ListLocalBook.js
@@ -2,66 +2,68 @@ import { Container, Row, Col, Pagination } from "react-bootstrap";
 import * as library from "../api/firestore";
 import ItemLocalBook from "./ItemLocalBook";
 import { useState, useEffect, useRef } from "react";
+import Header from "../Header";
 
 function ListLocalBook() {
-    const [localBooks, setLocalBooks] = useState([]);
-    const [page, setPage] = useState(1);
-    const [pageCount, setPageCount] = useState(1);
-    const pageSize = 12;
+  const [localBooks, setLocalBooks] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
+  const pageSize = 12;
 
-    useEffect(() => {
-        library.getCountListLocalLibrary().then((res) => {
-            setPageCount(Math.ceil(res / pageSize));
-        });
-    }, []);
-    useEffect(() => {
-        library.getListLocalLibrary(page, pageSize).then((res) => {
-            setLocalBooks(res);
-        });
-    }, [page]);
+  useEffect(() => {
+    library.getCountListLocalLibrary().then((res) => {
+      setPageCount(Math.ceil(res / pageSize));
+    });
+  }, []);
+  useEffect(() => {
+    library.getListLocalLibrary(page, pageSize).then((res) => {
+      setLocalBooks(res);
+    });
+  }, [page]);
 
-    let pageItem = [];
-    for (let i = 1; i <= pageCount; i++) {
-        pageItem.push(
-            <Pagination.Item
-                key={i}
-                active={i === page}
-                onClick={() => {
-                    setPage(i);
-                }}
-            >
-                {i}
-            </Pagination.Item>
-        );
-    }
-
-    return (
-        <div>
-            <Container className="my-5">
-                <Pagination className="justify-content-center">
-                    <Pagination.Prev
-                        onClick={() => {
-                            if (page > 1) setPage(page - 1);
-                        }}
-                    />
-                    {pageItem}
-                    <Pagination.Next
-                        onClick={() => {
-                            if (page < pageCount) setPage(page + 1);
-                        }}
-                    />
-                </Pagination>
-
-                <Row className="g-2">
-                    {localBooks.map((bookData, index) => (
-                        <Col key={index}>
-                            <ItemLocalBook bookData={bookData} />
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        </div>
+  let pageItem = [];
+  for (let i = 1; i <= pageCount; i++) {
+    pageItem.push(
+      <Pagination.Item
+        key={i}
+        active={i === page}
+        onClick={() => {
+          setPage(i);
+        }}
+      >
+        {i}
+      </Pagination.Item>
     );
+  }
+
+  return (
+    <div>
+      <Header />
+      <Container className="my-5">
+        <Pagination className="justify-content-center">
+          <Pagination.Prev
+            onClick={() => {
+              if (page > 1) setPage(page - 1);
+            }}
+          />
+          {pageItem}
+          <Pagination.Next
+            onClick={() => {
+              if (page < pageCount) setPage(page + 1);
+            }}
+          />
+        </Pagination>
+
+        <Row className="g-2">
+          {localBooks.map((bookData, index) => (
+            <Col key={index}>
+              <ItemLocalBook bookData={bookData} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default ListLocalBook;
