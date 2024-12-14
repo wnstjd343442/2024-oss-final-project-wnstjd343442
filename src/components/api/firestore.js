9eef50418f46eb1773870fc8d35f2925ac512ed4
kimjavascript <wnstjd343442@gmail.com>
2024-12-14 20:45:07 +0900

    진짜 완성

--- src/components/api/firestore.js
+++ src/components/api/firestore.js
@@ -2,155 +2,152 @@ import axios from "axios";
 import QueryString from "qs";
 
 axios.defaults.paramsSerializer = (params) => {
-    return QueryString.stringify(params, {
-        arrayFormat: "repeat",
-        allowDots: true,
-    });
+  return QueryString.stringify(params, {
+    arrayFormat: "repeat",
+    allowDots: true,
+  });
 };
 
 async function getLocalLibrary(isbn) {
-    let res = await axios.get(
-        process.env.REACT_APP_FIRESTORE_URL + "/documents/library/" + isbn
-    );
+  let res = await axios.get(
+    process.env.REACT_APP_FIRESTORE_URL + "/documents/library/" + isbn
+  );
 
-    return otd(res.data.fields);
+  return otd(res.data.fields);
 }
 async function createLocalLibrary(bookData) {
-    let res = axios.post(
-        process.env.REACT_APP_FIRESTORE_URL + "/documents/library/",
-        { fields: dto(bookData) },
-        { params: { documentId: bookData.isbn } }
-    );
+  let res = axios.post(
+    process.env.REACT_APP_FIRESTORE_URL + "/documents/library/",
+    { fields: dto(bookData) },
+    { params: { documentId: bookData.isbn } }
+  );
 
-    return res;
+  return res;
 }
 
 async function updateLocalLibrary(isbn, bookData) {
-    let res = axios.patch(
-        process.env.REACT_APP_FIRESTORE_URL + "/documents/library/" + isbn,
-        {
-            fields: dto(bookData),
-        },
-        { params: { updateMask: { fieldPaths: Object.keys(bookData) } } }
-    );
+  let res = axios.patch(
+    process.env.REACT_APP_FIRESTORE_URL + "/documents/library/" + isbn,
+    {
+      fields: dto(bookData),
+    },
+    { params: { updateMask: { fieldPaths: Object.keys(bookData) } } }
+  );
 
-    return res;
+  return res;
 }
 
 async function delLocalLibrary(isbn) {
-    let res = axios.delete(
-        process.env.REACT_APP_FIRESTORE_URL + "/documents/library/" + isbn
-    );
+  let res = axios.delete(
+    process.env.REACT_APP_FIRESTORE_URL + "/documents/library/" + isbn
+  );
 
-    return res;
+  return res;
 }
 
 async function getListLocalLibrary(page, search_query = null, pageSize = 12) {
-    const query = {
-        structuredQuery: {
-            offset: (page - 1) * 12,
-            limit: pageSize,
-            where: {
-                compositeFilter: {
-                    op: "AND",
-                    filters: qtf(search_query),
-                },
-            },
-            from: [{ collectionId: "library" }],
+  const query = {
+    structuredQuery: {
+      offset: (page - 1) * 12,
+      limit: pageSize,
+      where: {
+        compositeFilter: {
+          op: "AND",
+          filters: qtf(search_query),
         },
-    };
+      },
+      from: [{ collectionId: "library" }],
+    },
+  };
 
-    const res = await axios.post(
-        process.env.REACT_APP_FIRESTORE_URL + "/documents:runQuery",
-        query
-    );
+  const res = await axios.post(
+    process.env.REACT_APP_FIRESTORE_URL + "/documents:runQuery",
+    query
+  );
 
-    if (res.length == 1) if (!res.data[0].hasOwnProperty("document")) return []; // 검색 결과 없음
-    if (page > 1) return res.data.slice(1).map((o) => otd(o.document.fields));
-    else return res.data.map((o) => otd(o.document.fields));
+  if (res.length === 1) if (!res.data[0].hasOwnProperty("document")) return []; // 검색 결과 없음
+  if (page > 1) return res.data.slice(1).map((o) => otd(o.document.fields));
+  else return res.data.map((o) => otd(o.document.fields));
 }
 
 async function getCountListLocalLibrary(search_query) {
-    const res = await axios.post(
-        process.env.REACT_APP_FIRESTORE_URL + "/documents:runAggregationQuery",
-        {
-            structuredAggregationQuery: {
-                aggregations: [{ count: {} }],
-                structuredQuery: {
-                    where: {
-                        compositeFilter: {
-                            op: "AND",
-                            filters: qtf(search_query),
-                        },
-                    },
-                    from: [{ collectionId: "library" }],
-                },
+  const res = await axios.post(
+    process.env.REACT_APP_FIRESTORE_URL + "/documents:runAggregationQuery",
+    {
+      structuredAggregationQuery: {
+        aggregations: [{ count: {} }],
+        structuredQuery: {
+          where: {
+            compositeFilter: {
+              op: "AND",
+              filters: qtf(search_query),
             },
-        }
-    );
+          },
+          from: [{ collectionId: "library" }],
+        },
+      },
+    }
+  );
 
-    return res.data[0].result.aggregateFields.field_1.integerValue;
+  return res.data[0].result.aggregateFields.field_1.integerValue;
 }
 
 function qtf(search_query) {
-    const search_filter = [];
-    if (search_query.hasOwnProperty("star"))
-        search_filter.push({
-            fieldFilter: {
-                op: "GREATER_THAN_OR_EQUAL",
-                value: { doubleValue: search_query.star },
-                field: { fieldPath: "star" },
-            },
-        });
-    if (
-        search_query.hasOwnProperty("author") &&
-        search_query.author.length > 0
-    ) {
-        search_filter.push({
-            fieldFilter: {
-                op: "EQUAL",
-                value: { stringValue: search_query.author },
-                field: { fieldPath: "author" },
-            },
-        });
-    }
-    return search_filter;
+  const search_filter = [];
+  if (search_query.hasOwnProperty("star"))
+    search_filter.push({
+      fieldFilter: {
+        op: "GREATER_THAN_OR_EQUAL",
+        value: { doubleValue: search_query.star },
+        field: { fieldPath: "star" },
+      },
+    });
+  if (search_query.hasOwnProperty("author") && search_query.author.length > 0) {
+    search_filter.push({
+      fieldFilter: {
+        op: "EQUAL",
+        value: { stringValue: search_query.author },
+        field: { fieldPath: "author" },
+      },
+    });
+  }
+  return search_filter;
 }
 
 function dto(bookData) {
-    let fields = {};
-    Object.keys(bookData).forEach((fieldName) => {
-        if (bookData[fieldName] instanceof Date)
-            fields[fieldName] = {
-                timestampValue: bookData[fieldName].toISOString(),
-            };
-        else if (typeof bookData[fieldName] == "string")
-            fields[fieldName] = { stringValue: bookData[fieldName] };
-        else if (typeof bookData[fieldName] == "number")
-            fields[fieldName] = {
-                doubleValue: bookData[fieldName].toString(),
-            };
-    });
-    return fields;
+  let fields = {};
+  Object.keys(bookData).forEach((fieldName) => {
+    if (bookData[fieldName] instanceof Date)
+      fields[fieldName] = {
+        timestampValue: bookData[fieldName].toISOString(),
+      };
+    else if (typeof bookData[fieldName] == "string")
+      fields[fieldName] = { stringValue: bookData[fieldName] };
+    else if (typeof bookData[fieldName] == "number")
+      fields[fieldName] = {
+        doubleValue: bookData[fieldName].toString(),
+      };
+  });
+  return fields;
 }
 
 function otd(objectData) {
-    let bookData = {};
-    Object.keys(objectData).forEach((fieldName) => {
-        let type = Object.keys(objectData[fieldName])[0];
-        let res = objectData[fieldName][type];
-        if (type == "doubleValue") res = Number(res);
-        else if (type == "timestampValue") res = new Date(res);
-        bookData[fieldName] = res;
-    });
-    return bookData;
+  let bookData = {};
+  Object.keys(objectData).forEach((fieldName) => {
+    let type = Object.keys(objectData[fieldName])[0];
+    let res = objectData[fieldName][type];
+    if (type == "doubleValue") res = Number(res);
+    else if (type == "timestampValue") res = new Date(res);
+    bookData[fieldName] = res;
+  });
+  return bookData;
 }
 
 export {
-    getLocalLibrary,
-    getCountListLocalLibrary,
-    createLocalLibrary,
-    getListLocalLibrary,
-    updateLocalLibrary,
-    delLocalLibrary,
+  getLocalLibrary,
+  getCountListLocalLibrary,
+  createLocalLibrary,
+  getListLocalLibrary,
+  updateLocalLibrary,
+  delLocalLibrary,
 };
